a90a9fdf990b2082a352e0d200bbddefb51f1dba
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-22 22:06:12 +0400

    Create index.ts

---
+++ 04-SPECIAL-TYPES-ANY/index.ts
@@ -0,0 +1,27 @@
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
+
+let salary: number;
+salary = 5000;
+
+const userData =
+  '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
+
+const userObj: {
+  isBirthdayData: boolean;
+  ageData: number;
+  userNameData: string;
+} = JSON.parse(userData);
+
+console.log(userObj.userNameData);
+
+// function LogMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return "Error!";
+//   }
+// }
+
+// LogMsg(isBirthdayData, userNameData, 40);
