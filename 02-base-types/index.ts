d9d2a7dd3f5c9a908ec36c6826ecdadc156766f7
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-22 21:15:02 +0400

    Create index.ts

---
+++ 02-base-types/index.ts
@@ -0,0 +1,26 @@
+// let userName: string = "Name";
+// userName = "ksdlfsk";
+
+//                          - string
+//   BASIC TYPES            - number (int, float, complex)
+//                          - boolean
+
+const isBirthday: boolean = true;
+const age: number = 40;
+const userName: string = "John";
+
+if (isBirthday) {
+  console.log(`Congrats ${userName}, age: ${age + 1}`);
+}
+
+// Congrats John, age: 41
+
+const isBirthday_ERROR = 1;
+const age_ERROR = "40";
+const userName_ERROR = "John";
+
+if (isBirthday) {
+  console.log(`Congrats ${userName_ERROR}, age: ${age_ERROR + 1}`);
+}
+
+// Congrats John, age: 401
