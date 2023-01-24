5b5512c3645507175ff3a4053273856cfe71c142
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-24 16:52:38 +0400

    Create type-undefined-null.ts / Commented

---
+++ 07-TYPE-UNDEFINED-NULL/type-undefined-null.ts
@@ -0,0 +1,18 @@
+// const test1: null = null;
+// const test2: any = null;
+
+// const test3: string = null;    ----  ERROR
+// const test4: number = null;    ----  ERROR
+
+// let smth; -- any;
+
+// function getRndData() {
+//   if (Math.random() < 0.5) {
+//     return null;
+//   } else {
+//     return "   Some Data   ";
+//   }
+// }
+
+// const data = getRndData();
+// const trimmedData = data ? data.trim() : null;
