eeaff63dbbae01dacecef38885fa799b86d9bbe6
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-24 16:16:50 +0400

    Update basic-types.ts / Commented

--- 02-BASIC-TYPES/basic-types.ts
+++ 02-BASIC-TYPES/basic-types.ts
@@ -5,22 +5,22 @@
 //   BASIC TYPES            - number (int, float, complex)
 //                          - boolean
 
-const isBirthday: boolean = true;
-const age: number = 40;
-const userName: string = "John";
+// const isBirthday: boolean = true;
+// const age: number = 40;
+// const userName: string = "John";
 
-if (isBirthday) {
-  console.log(`Congrats ${userName}, age: ${age + 1}`);
-}
+// if (isBirthday) {
+//   console.log(`Congrats ${userName}, age: ${age + 1}`);
+// }
 
 // Congrats John, age: 41
 
-const isBirthday_ERROR = 1;
-const age_ERROR = "40";
-const userName_ERROR = "John";
+// const isBirthday = 1;
+// const age = "40";
+// const userName = "John";
 
-if (isBirthday_ERROR) {
-  console.log(`Congrats ${userName_ERROR}, age: ${age_ERROR + 1}`);
-}
+// if (isBirthday) {
+//   console.log(`Congrats ${userName}, age: ${age + 1}`);
+// }
 
 // Congrats John, age: 401
