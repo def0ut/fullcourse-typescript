b76209b55b74bf0443fffdaeb47990caeb062aec
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-24 17:09:55 +0400

    Create bigint-symbol.ts / Commented

---
+++ 08-BIGINT-SYMBOL/bigint-symbol.ts
@@ -0,0 +1,13 @@
+// let id: symbol = Symbol("id");
+
+// const data = {
+//   [id]: 1,
+// };
+
+// console.log(data[id]); => 1
+
+// const num1: bigint = 1n;
+// const num2: bigint = 2n;
+
+// console.log(num1 + num2); => 5n
+// console.log(num1 + 5); => Error
