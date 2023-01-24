261a27f8ba7e25e65492126bc0d6fcc17700b007
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-24 16:36:07 +0400

    Update type-never.ts / Commented

---
+++ 06-TYPE-NEVER/type-never.ts
@@ -0,0 +1,17 @@
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
+
+// const createError = (msg: string) => {
+//   throw new Error(msg);
+// };
+
+// function LogMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return createError("Error!");
+//   }
+// }
+
+// LogMsg(isBirthdayData, userNameData, 40);
