d2ff33fa3a6c1bd906a8b5230feeacf27ba8fca4
Эмиль <akhtyamkinemil2004@gmail.com>
2023-01-24 16:20:12 +0400

    Update system-types-in-func.ts / Commented

--- 03-USING-SYSTEM-TYPES-IN-FUNCTION/system-types-in-func.ts
+++ 03-USING-SYSTEM-TYPES-IN-FUNCTION/system-types-in-func.ts
@@ -1,31 +1,27 @@
-const isBirthdayData: boolean = true;
-let ageData: number = 40;
-const userNameData: string = "John";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
 
-function LogMsg(isBirthday: boolean, userName: string, age: number): string {
-  if (isBirthday) {
-    return `Congrats ${userName}, age: ${age + 1}`;
-  } else {
-    return "Error!";
-  }
-}
+// function LogMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return "Error!";
+//   }
+// }
 
-LogMsg(isBirthdayData, userNameData, 40);
+// LogMsg(isBirthdayData, userNameData, 40);
 
-// Congrats John, age: 41
+// ---- Congrats John, age: 41
 
-const LogMsgConst = (
-  isBirthday: boolean,
-  userName: string,
-  age: number
-): string => {
-  if (isBirthday) {
-    return `Congrats ${userName}, age: ${age + 1}`;
-  } else {
-    return "Error!";
-  }
-};
+// const LogMsg = (isBirthday: boolean, userName: string, age: number): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return "Error!";
+//   }
+// };
 
-LogMsgConst(isBirthdayData, userNameData, 40);
+// LogMsg(isBirthdayData, userNameData, 40);
 
-// Будет тоже самое, что и с вызовом предыдущей функции!
+// ---- Congrats John, age: 41
